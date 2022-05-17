187080ddc84fcff5fdd0080ead36a73aceddec9d
priyanka-chauhan-0212 <105193255+priyanka-chauhan-0212@users.noreply.github.com>
2022-05-17 14:49:01 +0530

    update

--- src/Banner.js
+++ src/Banner.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./Banner.css";
+import cardimg from "./Assests/card-img.jpg";
 
 export default function Banner() {
   return (
     <div className="wrapper">
       <div className="card">
-        <img src={} alt=""></img>
+        <img className="cardimage" src={cardimg} alt=""></img>
+        <div className="bannercontainer">
+          <h4>
+            <b>John Doe</b>
+          </h4>
+          <p>Architect & Engineer</p>
+          <button>Coming Soon</button>
+        </div>
       </div>
     </div>
   );
