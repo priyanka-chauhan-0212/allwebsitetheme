53eb3e8bd9bbba0ff49c2f2e17e0220a756fb713
priyanka-chauhan-0212 <105193255+priyanka-chauhan-0212@users.noreply.github.com>
2022-05-17 15:01:01 +0530

    update

--- src/Banner.js
+++ src/Banner.js
@@ -6,7 +6,9 @@ export default function Banner() {
   return (
     <div className="wrapper">
       <div className="card">
-        <img className="cardimage" src={cardimg} alt=""></img>
+        <div className="card-image">
+          <img className="image" src={cardimg} alt=""></img>
+        </div>
         <div className="bannercontainer">
           <h4>
             <b>John Doe</b>
